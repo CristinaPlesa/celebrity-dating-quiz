5a9bf856bce390c7d1f87fd84da13f813d82955e
Cristina Plesa <cristinaplesa8@gmail.com>
2021-03-25 14:09:04 -0700

    Add conditional function for images and styling in css.
    Co-authored-by: Tori West <victoria_west@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   $("form#userInput").submit(function(event){
     const userHoroscope = $("#horoscope").val();
-    event.preventDefault();
-    if(userHoroscope==="Taurus"){
-      alert("You picked Taurus!");
+    if(userHoroscope=== "Taurus"|| userHoroscope==="Aries"|| userHoroscope==="Gemini"){
+      $(".Celebrities").hide();
+      $("#John").toggle(); 
+    } else if (userHoroscope==="Cancer"||userHoroscope==="Leo"||userHoroscope==="Virgo"){
+      $(".Celebrities").hide();
+      $("#Tod").toggle(); 
+    } else if (userHoroscope==="Libra" || userHoroscope==="Scorpio" || userHoroscope==="Sagittarius"){
+      $(".Celebrities").hide();
+      $("#Jenny").toggle();
+    } else {
+      $(".Celebrities").hide();
+      $("#Emelie").toggle();
     }
+    event.preventDefault();
+
   })
 })
